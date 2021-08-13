71f1dad20813e18354cbac77bcfcd42a106c6248
Riz <80712058+rizznn@users.noreply.github.com>
2021-08-13 01:00:36 -0400

    engineer class passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,22 @@
+const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
 test('creates an engineer object', () => {
-    const employee = new Engineer('Joe');
+    const employee = new Engineer('Joe', 3, 'engineer@email.com', 'JoeGithubUsername', 'Engineer');
 
-    expect(employee.role).toBe('employee');
+    expect(employee.github).toBe('JoeGithubUsername');
+    expect(employee.role).toBe('Engineer');
+});
+
+test('sets the github username', () => {
+    const employee = new Engineer('Joe', 3, 'engineer@email.com', 'JoeGithubUsername');
+
+    expect(employee.getGithub()).toBe('JoeGithubUsername');
+});
+
+test('getRole() to return Engineer', () => {
+    const employee = new Engineer('Joe', 3, 'engineer@email.com', 'JoeGithubUsername');
+
+    expect(employee.getRole()).toBe('Engineer');
 });
