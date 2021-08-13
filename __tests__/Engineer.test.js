c67786b57101ca8cb300c37b2916ab58e7c4a58f
Riz <80712058+rizznn@users.noreply.github.com>
2021-08-13 00:08:57 -0400

    manager class passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,8 @@
 const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
 
 test('creates an engineer object', () => {
-    const employee = new Engineer('James');
-    
+    const employee = new Engineer('Joe');
+
     expect(employee.role).toBe('employee');
 });
