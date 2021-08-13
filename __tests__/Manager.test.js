8513af24e0200dda24a3e2bf5f60ae6a97333b4b
Riz <80712058+rizznn@users.noreply.github.com>
2021-08-12 23:47:37 -0400

    manager class passed except the first test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,16 +5,9 @@ const Manager = require('../lib/Manager');
 test('creates a manager object', () => {
     const employee = new Manager('Jane', 2, 'manager@email.com', 101, 'Manager');
 
-    // expect(employee.name).toBe('Jane');
     expect(employee.officeNumber).toBe(101);
     expect(employee.role).toBe('Manager');
 });
-
-// test('sets the name of the employee', () => {
-//     const employee = new Manager('Jane');
-
-//     expect(employee.getName()).toBe('Jane');
-// });
 
 test('sets the office number', () => {
     const employee = new Manager('Jane', 2, 'manager@email.com', 101);
@@ -25,5 +18,5 @@ test('sets the office number', () => {
 test('getRole() to return Manager', () => {
     const employee = new Manager('Jane', 2, 'manager@email.com', 101);
 
-    expect(employee.getRole()).toBe('manager');
+    expect(employee.getRole()).toBe('Manager');
 });
